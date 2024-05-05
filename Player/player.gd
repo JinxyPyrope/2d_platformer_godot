8e0b8fd907ab37bf32dcbe82f3cbb370cd746061
JinxyPyrope <jinxypy@yahoo.com>
2024-05-05 13:49:17 -0400

    Feat: Add Coyote Jump Timer to provide help with player jump

--- Player/player.gd
+++ Player/player.gd
@@ -10,6 +10,7 @@ const JUMP_VELOCITY = -300.0
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 @onready var animated_sprite_2d = $AnimatedSprite2D
+@onready var coyote_jump_timer = $CoyoteJumpTimer
 
 func _physics_process(delta):
 	apply_gravity(delta)
@@ -20,9 +21,17 @@ func _physics_process(delta):
 	handle_acceleration(input_axis, delta)
 	apply_friction(input_axis, delta)
 	update_animation(input_axis)
+	#This variaible is placed to chack to see if the player was on the floor before we move
+	var was_on_floor = is_on_floor()
 	#This method basically takes our velocity and moves the character by the velocity in the direction and slides it against any collisions. Basically as it shows move and slide agaist collisions
 	move_and_slide()
 	
+	#This checks if the character is sill on the floor after moving 
+	var just_left_ledge = was_on_floor and not is_on_floor() and velocity.y >= 0
+	
+	if just_left_ledge:
+		coyote_jump_timer.start()
+
 func apply_gravity(delta):
 	# Add the gravity.
 	# "Is_on_Floor is a func associated with "CharacterBody2D" (Or previously called KinematicBody2d) which is why we are using it here for gravity
@@ -32,10 +41,12 @@ func apply_gravity(delta):
 
 func handle_jump():
 	#"Ui_accept" is th espace key on yhe kayboard
-	if is_on_floor():
+	#The "Coyote_Jump_Timer"  checks if it's greater than 0 as well so we know that we can jump either on the floor or at a slight grace period after jumping
+	if is_on_floor() or coyote_jump_timer.time_left > 0.0:
 		if Input.is_action_just_pressed("ui_up"):
 			velocity.y = JUMP_VELOCITY
-	else:
+	#This is to checked to see if we are not on lthe floor then we can shorten the jump
+	if not is_on_floor():
 		if Input.is_action_just_released("ui_accept") and velocity.y < JUMP_VELOCITY / 2:
 			velocity.y = JUMP_VELOCITY / 2
 		
