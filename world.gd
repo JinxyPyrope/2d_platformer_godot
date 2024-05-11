bc79f3b2a01d87fef9e237b060ee852a68e718c8
JinxyPyrope <jinxypy@yahoo.com>
2024-05-11 13:52:45 -0400

    Feat: Create autotiles and slope tiles for physics and a system for tiles of level design.

--- world.gd
+++ world.gd
@@ -1,16 +1,11 @@
 extends Node2D
 
 
-@onready var polygon_2d = $StaticBody2D/CollisionPolygon2D/Polygon2D
-
-@onready var collision_polygon_2d = $StaticBody2D/CollisionPolygon2D
-
 @onready var level_completed = $CanvasLayer/LevelCompleted
 
 func _ready():
 	RenderingServer.set_default_clear_color(Color.BLACK)
 	#This mkaes the "Polygon2d" color go with the shape of the collision shape we made
-	polygon_2d.polygon = collision_polygon_2d.polygon
 	#From the level completed signal we  auotload, after being emitted when all hearts are collected this will come to connect
 	Events.level_completed.connect(show_level_completed)
 
