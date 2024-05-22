f7d7674f9ad72017b95284ce303f000033492811
JinxyPyrope <jinxypy@yahoo.com>
2024-05-22 18:20:27 -0400

    Feat: Add level timer to game for speedrun

---
+++ level_completed.gd
@@ -0,0 +1,15 @@
+extends ColorRect
+
+signal retry()
+signal next_level()
+
+@onready var retry_button = %RetryButton
+
+
+
+func _on_retry_button_pressed():
+	retry.emit()
+
+
+func _on_next_level_pressed():
+	next_level.emit()
--- world.gd
+++ world.gd
@@ -3,11 +3,16 @@ extends Node2D
 #Set up a variable that is for the net level when the player has completed one level
 @export var next_level: PackedScene
 
+var level_time = 0.0
+
+#set up the timer varibale 
+var start_level_msec = 0.0
 
 @onready var level_completed = $CanvasLayer/LevelCompleted
 @onready var start_in = %StartIn
 @onready var start_in_label = %StartInLabel
 @onready var animation_player = $AnimationPlayer
+@onready var level_time_label = %LevelTimeLabel
 
 
 func _ready():
@@ -22,7 +27,23 @@ func _ready():
 	await animation_player.animation_finished
 	get_tree().paused = false
 	start_in.visible = false
+	#"get_ticks_msec" keeps track of the amount of time passing as the game started
+	start_level_msec = Time.get_ticks_msec()
 	
+func _process(_delta):
+	#Shows and kees up with timer 
+	level_time = Time.get_ticks_msec() - start_level_msec
+	level_time_label.text = str(level_time / 1000.0)
+
+func retry():
+	await LevelTransition.fade_to_black()
+	get_tree().paused = false
+	get_tree().change_scene_to_file(scene_file_path)
+
+func go_to_next_level():
+	await LevelTransition.fade_to_black()
+	get_tree().paused = false
+	get_tree().change_scene_to_packed(next_level)
 	
 
 func show_level_completed():
@@ -30,12 +51,19 @@ func show_level_completed():
 	level_completed.show()	
 	#If there's no variable in next level then we don't movee to a next level
 	if not next_level is PackedScene: return
+	level_completed.retry_button.grab_focus()
 	get_tree().paused = true
-	await get_tree().create_timer(1.0).timeout
-	await LevelTransition.fade_to_black()
-	get_tree().paused = false
-	get_tree().change_scene_to_packed(next_level)
+	
+	
 
 	#This pauses the game when the final heart is collected
 	#get_tree().paused = true
 	
+
+
+func _on_level_completed_retry():
+	retry()
+
+
+func _on_level_completed_next_level():
+	go_to_next_level()
